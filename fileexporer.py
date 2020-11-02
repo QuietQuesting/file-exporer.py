87f5dfd42e62a43aad8eb6ad73eba06056e31f88
QuietQuesting <73032440+QuietQuesting@users.noreply.github.com>
2020-11-02 17:09:02 +0100

    Update fileexplorer.py with improvements and fixes
    Removed function parent_dir() replaced it with os.chdir('../'). Moved contents of paren_dir() in print_dir() where it's stil used
    Applied PEP8 Styling guide with pylint (code analyser)
    Fixed undefined variable bug in fselect()

--- fileexporer.py
+++ fileexporer.py
@@ -15,8 +15,6 @@ from platform import system
 from hashlib import sha1
 from send2trash import send2trash
 import configparser
-import pdb
-
 
 
 def read_argv() -> str:
@@ -33,9 +31,8 @@ def read_argv() -> str:
         decoder = sha1()
         decoder.update(bytes(argv, 'utf-8'))
         return decoder.hexdigest()
-    
-    else:
-        return argv
+
+    return argv
 
 
 def init_config(name='config.ini') ->str:
@@ -64,64 +61,73 @@ def get_starting_dir(config) ->bool:
 
     if not starting_dir or not os.path.isdir(starting_dir):
         return False
-    
-    else:
-        os.chdir(starting_dir)
-        return True
+
+    os.chdir(starting_dir)
+    return True
+
 
 def print_dir(folder=os.getcwd()) ->None:
     """Fancy prints directory and it's content."""
     intro = '\nContent of folder: {}'.format(folder)
-    
+
+    #get parent dir as a string
+    last_slash = folder.rfind('\\')
+    if last_slash != -1:
+    parent_dir = folder[:last_slash]
+
+    else:
+        raise OSError('Can\'t go back up!')
+
     print(intro)
     print('o', len(intro) * '-',sep='')
 
     print(f'| ...\tparent folder:\t{parent_dir(folder):20}')
-    
+
     for index, item in enumerate(os.listdir(folder)):
         path = folder + '\\' + item
-        
+
         if item[-4:] == '.lnk':
             _type = 'link/shortcut'
-            
+
         elif os.path.isfile(path):
             # Get Mimetype as tuple, want only first tuple item
             _type = mimetypes.guess_type(path, strict=False)[0]
 
-            if _type == None:
+            if _type is None:
                 _type = 'unknown/uncommon'
 
         else:
             _type = 'dir/folder'
-            
+
         print(f'| {index+1:02}.\t{item:20}{_type}')
-        
+
     return None
 
-    
+
 def input_num(number=-1)-> int:
     """ Accepts numbers within range of cwd.
     Special nums: 0 counts as abort. -1 as confirm."""
     dir_list = os.listdir(os.getcwd())
-        
-    while not (-1 < number <= len(dir_list)):
+
+    while not -1 < number <= len(dir_list):
 
         number = input('Enter your number now: ')
-        
+
         try:
             number = int(number)
 
-        except ValueError:  
+        except ValueError:
             print('That is not a number.')
-            
+
         if number == '':
             return -1
-            
+
         elif number in ('..', '.'):
             return -2
-            
-        print('Number out of range! Input number between 1 - {}.'.format(len(dir_list)))
-        
+
+        else:
+            print('Number out of range! Input number between 1 - {}.'.format(len(dir_list)))
+
     return number
 
 
@@ -132,77 +138,61 @@ def fselect(count=1) ->list[str]:
     print('Else enter ".." to go back up')
 
     selection = []
-    
+
     print(f'Please select {count} folder or files, one after the other')
     for i in range(count):
 
         selected = False
-        
+
         while not selected:
             num = input_num()
 
             if num == 0:
                 print('Aborting process.')
                 return 0
-            
+
             elif num == -1:
                 print('Current folder is selected!')
-                selection.append(f)
+                selection.append(os.getcwd())
                 selected = True
 
             elif num == -2:
-                os.chdir(parent_dir())
+                os.chdir('../') # parent dir
                 print("Going to parent folder.")
                 print_dir()
 
             else:   # open folder / ask if user wants to select this file
                 dir_list = os.listdir(os.getcwd())
-                file = dir_list[num-1] # file or folder
-                
-                if os.path.isfile(f):
-                    choice = input(f'Do you want to select {f}? (y,n)')
-                    
+                file_name = dir_list[num-1] # file or folder
+                f_path = os.getcwd() + '\\' + file_name
+
+                if os.path.isfile(file):
+                    choice = input(f'Do you want to select {file_name}? (y,n)')
+
                     if choice.replace(' ', '').lower() in ('y', 'yes', 'yeah'):
                         print('File selected')
-                        selection.append(f)
+                        selection.append(f_path)
                         selected = True
-                    
+
                     else:
                         print('File not selected.\n')
                 else:
-                    os.chdir(os.getcwd() + '\\' + file)
-                    print('Going into selected folder:')
-                    print(f)
+                    os.chdir(f_path)
+                    print('Going into selected folder: '.format(f_path))
                     print_dir()
     return selected
-        
-            
 
-def fopen(number) -> bool:
-    """Opens n-st file or folder in cwd.
+
+
+def fopen(number, directory=os.getcwd()) -> bool:
+    """Opens n-st file in directory.
         Todo: test if symlinks (.lnk) open"""
-    direcory = os.getcwd()
-    
-    path = direcory + '\\' + dir_list[number-1]
-    
-    if os.path.isfile(path):
-        
-        return False
-    
-    else:
-        list_dir()
-        os.chdir(path)
-        return True
 
-            
-def parent_dir(directory=os.getcwd) ->str:
-    """Returns the parent-directory as a string."""
-    last_slash = directory.rfind('\\')
-    
-    if last_slash != -1:
-        return directory[:last_slash]
-    
-    raise OSError('Can\'t go back up!')
+    path = directory + '\\' + os.listdir(directory)number-1]
+
+    if os.path.isfile(path):
+        return True
+    return  False
 
 
 def print_intro() ->None:
@@ -211,7 +201,7 @@ def print_intro() ->None:
     print('Welcome to my file exporer!')
     print('===========================')
     print('Please choose one of the following options:\n')
-    
+
     print_options(intro=True)
     print_dir()
     return None
@@ -226,11 +216,10 @@ def print_options(intro=False) ->None:
     print('co / copy to copy file/folder')
     print('m / move to crop file/folders')
     print('del to delete file/folders')
-    
     return None
 
-    
-def main():
+
+def main() ->None:
     """ Main script flow of the file explorer.
     In case you want to prompt the user to choose a folder/file use fselect. """
     config = init_config()
@@ -244,33 +233,31 @@ def main():
 
         if option.isdecimal():
             option = int(option)
-            
-            file = input_num(option)
             dir_list = os.listdir(os.getcwd())
-            
+
             if os.path.isdir(dir_list[option-1]):
-                print('Opening folder: {}'.format(dir_list[option-1]))
-                os.chdir(os.getcwd() + '\\' + dir_list[option-1])
+                file_name = dir_list[option-1]
+                print('Opening folder: {}'.format(file_name))
+                os.chdir(os.getcwd() + '\\' + file_name)
                 print_dir()
-                
+
             else:
                 print('Opening file: ')
 
         elif option in ('.', '..'):
             print('Opening parent folder.')
             print_dir()
-            os.chdir(parent_dir())    
-            
+            os.chdir('../') # change to parent dir
+
         elif option in ('copy', 'c'):
             print('Option: copy file or folder')
             print(f'selected: {fselect(2)}')
-        
+
         elif option in ('move', 'm'):
             print('Option: move file or folder')
             print(f'selected: {fselect(2)}')
-            pass
-        
-        elif option in ('del', 'd'): 
+
+        elif option in ('del', 'd'):
             print('Option: delete file or folder')
             print(f'selected: {fselect()}')
 
@@ -278,11 +265,9 @@ def main():
             print_options()
         else:
             print('Nothing.')
-            
+
     return None
 
 
 if __name__ == '__main__':
     main()
-            
-        
